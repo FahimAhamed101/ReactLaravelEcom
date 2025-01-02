7e61f5290d9240178a65afb59e265b2b0bfa6984
fahim <fahimahamedweb@gmail.com>
2025-01-02 19:33:31 +0600

    api register done

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,30 +7,41 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'address',
+        'city',
+        'zip_code',
+        'country',
+        'phone_number',
+        'profile_image',
+        'profile_completed'
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
         'remember_token',
+    ];
+
+    protected $appends = [
+        'image_path',
     ];
 
     /**
@@ -44,5 +55,17 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+  
+
+    public function getImagePathAttribute()
+    {
+        if($this->profile_image)
+        {
+            return asset($this->profile_image);
+        }else {
+            return 'https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_1280.png';
+        }
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,10 @@
 <?php
-
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-use App\Http\Controllers\Api\UserController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
